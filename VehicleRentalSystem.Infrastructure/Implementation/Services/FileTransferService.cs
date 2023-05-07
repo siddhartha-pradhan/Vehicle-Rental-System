0770cf6237083d6eaf7fd350c6fa211314d9a256
Siddhartha <siddhartha.pradhan.ix@gmail.com>
2023-05-07 14:18:36 +0545

    System Finalization

--- VehicleRentalSystem.Infrastructure/Implementation/Services/FileTransferService.cs
+++ VehicleRentalSystem.Infrastructure/Implementation/Services/FileTransferService.cs
@@ -71,7 +71,7 @@ public class FileTransferService : IFileTransferService
             file.CopyTo(fileStreams);
         }
 
-        return @$"images\{type.ToLower()}\" + fileName + extension;
+        return @$"\images\{type.ToLower()}\" + fileName + extension;
     }
 
     public byte[] ImageByte(IFormFile file)
